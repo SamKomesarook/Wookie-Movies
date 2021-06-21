78b5688817590b73eb570a9b93e9f99bc80d45d0
sam <skomesarook@protonmail.com>
2021-06-21 17:16:08 +1000

    fix: restyle modal

--- Sample/__tests__/Header-test.js
+++ Sample/__tests__/Header-test.js
@@ -4,33 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import ItemModal from '../app/components/ItemModal/ItemModal';
+import Header from '../app/components/Header/Header';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(
-    <ItemModal
-      item={{
-        item: {
-          backdrop:
-            'https://wookie.codesubmit.io/static/backdrops/d6822b7b-48bb-4b78-ad5e-9ba04c517ec8.jpg',
-          cast: ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart'],
-          classification: '13+',
-          director: 'Christopher Nolan',
-          genres: ['Action', 'Crime', 'Drama'],
-          id: 'd6822b7b-48bb-4b78-ad5e-9ba04c517ec8',
-          imdb_rating: 9,
-          length: '2h 32min',
-          overview:
-            'Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.',
-          poster:
-            'https://wookie.codesubmit.io/static/posters/d6822b7b-48bb-4b78-ad5e-9ba04c517ec8.jpg',
-          released_on: '2008-07-16T00:00:00',
-          slug: 'the-dark-knight-2008',
-          title: 'The Dark Knight',
-        },
-      }}
-    />,
-  );
+  renderer.create(<Header />);
 });
--- Sample/app/components/ItemModal/ItemModal.js
+++ Sample/app/components/ItemModal/ItemModal.js
@@ -5,11 +5,15 @@ import {
   TouchableOpacity,
   View,
   ImageBackground,
+  Image,
+  SafeAreaView,
+  ScrollView
 } from 'react-native';
 
 const ItemModal = props => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
       <View style={styles.topSection}>
         <ImageBackground
           source={{
@@ -21,20 +25,58 @@ const ItemModal = props => {
           </TouchableOpacity>
         </ImageBackground>
       </View>
-
       <View style={styles.bottomSection}>
-        <TouchableOpacity onPress={props.reset}>
-          <Text style={styles.Title}>{props.item.title}</Text>
-        </TouchableOpacity>
+        <View style={styles.row}>
+          <Image
+            source={{
+              uri: props.item.poster,
+            }}
+            style={styles.poster}
+            height={160}
+            width={120}
+          />
+          <View style={styles.movieTitle}>
+            <Text style={styles.title}>{props.item.title}</Text>
+          </View>
+        </View>
+        <Text style={styles.title}>
+          {new Date(props.item.released_on).toDateString()}
+        </Text>
+        <Text style={styles.title}>{props.item.length}</Text>
+        <Text style={styles.title}>{props.item.director}</Text>
       </View>
-    </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
-    overflow: 'hidden',
+  safeContainer: {
     flex: 1,
+    flexGrow: 1,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    top: -50,
+    marginLeft: 20,
+  },
+  movieTitle: {
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  background: {
+    overflow: 'visible',
+    flex: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    opacity: 0.5,
+  },
+  poster: {
+    overflow: 'visible',
   },
   topSection: {
     flex: 1,
@@ -46,7 +88,7 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     textAlign: 'left',
   },
-  Title: {
+  title: {
     fontSize: 28,
     fontWeight: 'bold',
     marginLeft: 5,
